ca9d90826d18ed7a4260a56e2d1425dd74b24fd9
Nathan Dautenhahn <dautenh1@illinois.edu>
2014-08-26 12:45:44 -0500

    Added some common macros and such for the NK internal code.

---
+++ sys/nk/common.h
@@ -0,0 +1,72 @@
+/*===-- LICENSE ------------------------------------------------------------===
+ * 
+ * University of Illinois/NCSA Open Source License 
+ *
+ * Copyright (C) 2014, The Board of Trustees of the University of Illinois.
+ * All rights reserved. 
+ *
+ * Developed by: 
+ *
+ *    Research Group of Professor Vikram Adve in the Department of Computer
+ *    Science The University of Illinois at Urbana-Champaign
+ *    http://web.engr.illinois.edu/~vadve/Home.html
+ *
+ * Copyright (c) 2014, Nathan Dautenhahn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the Software), to deal
+ * with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions: 
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimers. 
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimers in the documentation
+ * and/or other materials provided with the distribution.  Neither the names of
+ * Sam King or the University of Illinois, nor the names of its contributors
+ * may be used to endorse or promote products derived from this Software
+ * without specific prior written permission. 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE. 
+ *
+ *===-----------------------------------------------------------------------===
+ *
+ *       Filename:  common.h
+ *
+ *    Description:  Holds various things that are in many files
+ *
+ *===-----------------------------------------------------------------------===
+ */
+
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+/* 
+ * Defines for #if #endif blocks for commenting out lines of code
+ */
+/* Used to denote unimplemented code */
+#define NOT_YET_IMPLEMENTED 0   
+
+/* Used to denote obsolete code that hasn't been deleted yet */
+#define OBSOLETE            0   
+
+/* Define whether to enable DEBUG blocks #if statements */
+#define DEBUG               0
+
+/* Things that are in the PerspicuOS tree, but haven't been fully moved */
+#define NOT_PORTED_YET      0
+
+/* Things to review further and potentially remove */
+#define REVIEW_OBSOLETE     0
+
+#endif /* __COMMON_H__ */
+
