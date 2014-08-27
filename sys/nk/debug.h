ffed3c418d4895f88bcd97be64d5fcc383e5388b
Nathan Dautenhahn <dautenh1@illinois.edu>
2014-08-27 09:37:01 -0500

    This has some simple debugging support for NK dev.

---
+++ sys/nk/debug.h
@@ -0,0 +1,65 @@
+/*===-- LICENSE ------------------------------------------------------------===
+ * 
+ * University of Illinois/NCSA Open Source License 
+ *
+ * Copyright (C) 2014, The Board of Trustees of the University of Illinois.
+ * All rights reserved. 
+ *
+ * Developed by: 
+ *
+ *    Research Group of Professor Vikram Adve in the Department of Computer
+ *    Science The University of Illinois at Urbana-Champaign
+ *    http://web.engr.illinois.edu/~vadve/Home.html
+ *
+ * Copyright (c) 2014, Nathan Dautenhahn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the Software), to deal
+ * with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions: 
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimers. 
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimers in the documentation
+ * and/or other materials provided with the distribution.  Neither the names of
+ * Sam King or the University of Illinois, nor the names of its contributors
+ * may be used to endorse or promote products derived from this Software
+ * without specific prior written permission. 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE. 
+ *
+ *===-----------------------------------------------------------------------===
+ *
+ *       Filename:  debug.h
+ *
+ *    Description:  Debug support
+ *
+ *===-----------------------------------------------------------------------===
+ */
+
+#ifndef __NK_DEBUG_H_
+#define __NK_DEBUG_H_
+
+#include <sys/libkern.h>
+
+#undef NKDEBUG             /* undef it, just in case */
+#define NKDEBUG(fname, fmt, args...)                 \
+    printf("___Nested Kernel___ <%s> ", #fname);      \
+    printf(fmt, ## args);                               \
+    printf("\n")
+
+#undef NKDEBUGG
+#define NKDEBUGG(fname, fmt, args...) /* nothing: it's a placeholder */
+
+#endif /* _NK_DEBUG_H */
+
