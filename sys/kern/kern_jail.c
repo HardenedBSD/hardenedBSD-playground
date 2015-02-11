bc62508d57ee9ee305417b3689dad56be75fedbe
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-02-11 16:55:18 -0500

    Fix mismerge

--- sys/kern/kern_jail.c
+++ sys/kern/kern_jail.c
@@ -4690,8 +4690,8 @@ db_show_prison(struct prison *pr)
 	   pr->pr_hardening.hr_pax_map32_enabled);
 	db_printf(" .hr_pax_procfs_harden           = %d\n",
 	   pr->pr_hardening.hr_pax_procfs_harden);
-	db_printf(" .hr_pax_mprotect_exec           = %d\n",
-	   pr->pr_hardening.hr_pax_mprotect_exec);
+	db_printf(" .hr_pax_mprotect_status         = %d\n",
+	   pr->pr_hardening.hr_pax_mprotect_status);
 	db_printf(" .hr_pax_ptrace_hardening_status = %d\n",
 	   pr->pr_hardening.hr_pax_ptrace_hardening_status);
 	db_printf(" }\n");
