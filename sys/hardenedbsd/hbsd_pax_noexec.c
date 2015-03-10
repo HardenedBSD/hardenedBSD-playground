137b8764f739b2391939d5ce8ec07e715ad1ffed
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-03-10 22:12:18 +0100

    HBSD NOEXEC: avoid merge conflict in hbsd_pax_noexec.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/hardenedbsd/hbsd_pax_noexec.c
+++ sys/hardenedbsd/hbsd_pax_noexec.c
@@ -275,10 +275,6 @@ pax_pageexec_setup_flags(struct image_params *imgp, u_int mode)
 		if (mode & PAX_NOTE_NOPAGEEXEC) {
 			flags &= ~PAX_NOTE_PAGEEXEC;
 			flags |= PAX_NOTE_NOPAGEEXEC;
-			pax_log_pageexec(imgp->proc, PAX_LOG_DEFAULT,
-"PAGEEXEC is opt-out, and executable has explicitly disabled PAGEEXEC!");
-			pax_ulog_pageexec(
-"PAGEEXEC is opt-out, and executable has explicitly disabled PAGEEXEC!\n");
 		} else {
 			flags |= PAX_NOTE_PAGEEXEC;
 			flags &= ~PAX_NOTE_NOPAGEEXEC;
@@ -402,10 +398,6 @@ pax_mprotect_setup_flags(struct image_params *imgp, u_int mode)
 		if (mode & PAX_NOTE_NOMPROTECT) {
 			flags &= ~PAX_NOTE_MPROTECT;
 			flags |= PAX_NOTE_NOMPROTECT;
-			pax_log_mprotect(imgp->proc, PAX_LOG_DEFAULT,
-"MPROTECT is opt-out, and executable has explicitly disabled MPROTECT!");
-			pax_ulog_mprotect(
-"MPROTECT is opt-out, and executable has explicitly disabled MPROTECT!\n");
 		} else {
 			flags |= PAX_NOTE_MPROTECT;
 			flags &= ~PAX_NOTE_NOMPROTECT;
