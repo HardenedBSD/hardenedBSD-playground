5931820d5e099c8f1b73bc23f899398ca9a58936
Shawn Webb <shawn.webb@hardenedbsd.org>
2016-01-19 23:26:39 -0500

    HBSD: Grab the stack protection level from the ELF auxiliary vector.
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -515,6 +515,10 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     dbg("initializing thread locks");
     lockdflt_init();
 
+    if (aux_info[AT_STACKPROT] != NULL &&
+      aux_info[AT_STACKPROT]->a_un.a_val != 0)
+	    stack_prot = aux_info[AT_STACKPROT]->a_un.a_val;
+
     /*
      * Load the main program, or process its program header if it is
      * already loaded.
@@ -568,10 +572,6 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     }
     dbg("obj_main path %s", obj_main->path);
     obj_main->mainprog = true;
-
-    if (aux_info[AT_STACKPROT] != NULL &&
-      aux_info[AT_STACKPROT]->a_un.a_val != 0)
-	    stack_prot = aux_info[AT_STACKPROT]->a_un.a_val;
 
 #ifndef COMPAT_32BIT
     /*
