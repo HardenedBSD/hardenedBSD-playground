c8b2aa9a3dee55ebedb880abc5c560febb96e226
Shawn Webb <shawn.webb@hardenedbsd.org>
2017-02-09 12:57:36 -0500

    HBSD: Enable WITH_LLD_IS_LD on amd64
    This allows us to use CFI in base.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Sponsored-by:	SoldierX

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -258,7 +258,7 @@ __DEFAULT_YES_OPTIONS+=LLVM_LIBUNWIND
 .else
 __DEFAULT_NO_OPTIONS+=LLVM_LIBUNWIND
 .endif
-.if ${__T} == "aarch64"
+.if ${__T} == "aarch64" || ${__T} == "amd64"
 __DEFAULT_YES_OPTIONS+=LLD_IS_LD
 .else
 __DEFAULT_NO_OPTIONS+=LLD_IS_LD
