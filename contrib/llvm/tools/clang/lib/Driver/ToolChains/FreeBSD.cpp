0aed096e7750c71b0c3f7cb017a716d481880331
Shawn Webb <shawn.webb@hardenedbsd.org>
2017-08-24 10:16:56 -0400

    HBSD: Re-enable the SafeStack option for arm64
    This was lost in the llvm50 import.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Sponsored-by:	SoldierX

--- contrib/llvm/tools/clang/lib/Driver/ToolChains/FreeBSD.cpp
+++ contrib/llvm/tools/clang/lib/Driver/ToolChains/FreeBSD.cpp
@@ -381,6 +381,7 @@ SanitizerMask FreeBSD::getSupportedSanitizers() const {
   const bool IsX86_64 = getTriple().getArch() == llvm::Triple::x86_64;
   const bool IsMIPS64 = getTriple().getArch() == llvm::Triple::mips64 ||
                         getTriple().getArch() == llvm::Triple::mips64el;
+  const bool IsAarch64 = getTriple().getArch() == llvm::Triple::aarch64;
   SanitizerMask Res = ToolChain::getSupportedSanitizers();
   Res |= SanitizerKind::Address;
   Res |= SanitizerKind::Vptr;
@@ -388,7 +389,7 @@ SanitizerMask FreeBSD::getSupportedSanitizers() const {
     Res |= SanitizerKind::Leak;
     Res |= SanitizerKind::Thread;
   }
-  if (IsX86 || IsX86_64) {
+  if (IsX86 || IsX86_64 || IsAarch64) {
     Res |= SanitizerKind::SafeStack;
   }
   return Res;
